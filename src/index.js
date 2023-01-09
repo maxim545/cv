d7e3eece113e59a9c7c39efc21499e2ba1a29e18
maxim545 <maxximc95@gmail.com>
2023-01-09 18:42:09 +0300

    feat: add adaptive

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ function showSlides(n) {
     if (n < 1) {
         slideIndex = slides.length;
     }
-    for (let i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i += 1) {
         slides[i].style.display = 'none';
     }
     slides[slideIndex - 1].style.display = 'block';
